7ccaa8678d6eff506d3aa967fe67257989fd5bda
Omar Lomeli <o.a.1027@outlook.com>
2020-03-03 12:14:25 -0800

    fix bug on email test and subcribe button padding

--- Js/main.js
+++ Js/main.js
@@ -1,6 +1,7 @@
 document.addEventListener("DOMContentLoaded", () => {
 
 const click = document.getElementById('input-subscribe-button');
+const input = document.getElementById('test');
 
 click.addEventListener('click', valEmail);
 
@@ -8,7 +9,7 @@ click.addEventListener('click', valEmail);
 
 function valEmail(email){
     event.preventDefault();
-    if (/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(email) ) {
+    if (/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(input.value) ) {
         alert('Thanks for subscribing');
     }else {
     alert(' A valid Email is required');
@@ -16,6 +17,7 @@ function valEmail(email){
 }
 
 });
+
 
 
 
